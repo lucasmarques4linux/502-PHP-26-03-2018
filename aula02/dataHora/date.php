40f76dc82faedcd0862f3774b50e85b4af2812f5
Lucas Marques <lucasmarques73@hotmail.com>
2018-03-27 22:27:24 -0300

    fim aula02

---
+++ aula02/dataHora/date.php
@@ -0,0 +1,38 @@
+<?php 
+
+echo date('d/m/y');
+
+echo "<hr>";
+
+echo date('m-d-y');
+echo "<hr>";
+
+echo date('m-d-Y');
+
+echo "<hr>";
+
+echo date('d/m/Y');
+
+echo "<hr>";
+
+echo date('D/M/Y');
+
+echo "<hr>";
+
+echo date('d/m/Y H:i:s');
+
+echo "<hr>";
+
+echo date('l F');
+
+echo "<hr>";
+
+echo date('W');
+
+echo "<hr>";
+
+echo date('z');
+
+echo "<hr>";
+
+echo date('U');
